66ebf75584325cca7f05f79710140b8bc7586a62
xiaoQDR <347926431@qq.com>
2026-08-22 09:05:59 +0800

    Use Contain fit for Home Rive

--- src/app/GameApp.ts
+++ src/app/GameApp.ts
@@ -50,7 +50,7 @@ export class GameApp {
         src: RIVE.home,
         artboard: 'main',
         stateMachine: 'mainSM',
-        fit: Fit.Cover,
+        fit: Fit.Contain,
       })
     }
     catch (error) {
